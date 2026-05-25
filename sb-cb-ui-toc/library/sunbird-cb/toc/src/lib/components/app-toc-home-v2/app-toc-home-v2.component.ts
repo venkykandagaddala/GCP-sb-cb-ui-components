b2b79b8e265fc45f9f435f3fdbd644e4f7addcef
venkykandagaddala <venky.kandagaddala@gmail.com>
2026-05-25 14:44:37 +0530

    Show message when batch is filled with 80% seats

--- sb-cb-ui-toc/library/sunbird-cb/toc/src/lib/components/app-toc-home-v2/app-toc-home-v2.component.ts
+++ sb-cb-ui-toc/library/sunbird-cb/toc/src/lib/components/app-toc-home-v2/app-toc-home-v2.component.ts
@@ -972,7 +972,7 @@ export class AppTocHomeV2Component implements OnInit, OnDestroy, AfterViewChecke
     if (enrolled === undefined || currentBatchSize === undefined || currentBatchSize === 0) {
       return false
     }
-    return !this.isBatchFull && !(enrolled > currentBatchSize * 0.8)
+    return !this.isBatchFull && (enrolled > currentBatchSize * 0.8)
   }
 
   get isMobile(): boolean {
